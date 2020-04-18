260373d5a3ed94c7d7bc950c3bc7031acaacd728
Maxik73rus <myasoed73@mail.ru>
2020-04-18 20:12:48 +0300

    Pages
    Test

---
+++ js/main.js
@@ -0,0 +1,710 @@
+// Модальное окно
+
+// открыть по кнопке
+$('.js-button-campaign').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign').click(function() { 
+	$('.js-overlay-campaign').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//второе модальное окно
+
+$('.js-button-campaign2').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign2').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign2').click(function() { 
+	$('.js-overlay-campaign2').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign2');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign2').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//третье модальное окно
+
+
+$('.js-button-campaign3').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign3').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign3').click(function() { 
+	$('.js-overlay-campaign3').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign3');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign3').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+
+//четвертое окно
+
+$('.js-button-campaign4').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign4').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign4').click(function() { 
+	$('.js-overlay-campaign4').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign4');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign4').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//пятое окно
+
+$('.js-button-campaign5').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign5').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign5').click(function() { 
+	$('.js-overlay-campaign5').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign5');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign5').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//шестое окно
+
+$('.js-button-campaign6').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign6').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign6').click(function() { 
+	$('.js-overlay-campaign6').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign6');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign6').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//седьмое окно
+
+$('.js-button-campaign7').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign7').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign7').click(function() { 
+	$('.js-overlay-campaign7').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign7');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign7').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//восьмое окно
+
+$('.js-button-campaign8').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign8').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign8').click(function() { 
+	$('.js-overlay-campaign8').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign8');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign8').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//девятое окно
+
+$('.js-button-campaign9').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign9').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign9').click(function() { 
+	$('.js-overlay-campaign9').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign9');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign9').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//десятое окно
+
+$('.js-button-campaign10').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign10').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign10').click(function() { 
+	$('.js-overlay-campaign10').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign3');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign10').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//одиннадцатое окно
+
+$('.js-button-campaign11').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign11').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign11').click(function() { 
+	$('.js-overlay-campaign11').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign11');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign11').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//двенадцатое окно
+
+$('.js-button-campaign12').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign12').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign12').click(function() { 
+	$('.js-overlay-campaign12').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign12');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign12').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//тринадцатое окно
+
+$('.js-button-campaign13').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign13').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign13').click(function() { 
+	$('.js-overlay-campaign13').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign13');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign13').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//четырнадцатое окно
+
+$('.js-button-campaign14').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign14').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign14').click(function() { 
+	$('.js-overlay-campaign14').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign14');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign14').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//пятнадцатое окно
+
+$('.js-button-campaign15').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign15').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign15').click(function() { 
+	$('.js-overlay-campaign15').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign15');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign15').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//шестнадцатое окно
+
+$('.js-button-campaign16').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign16').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign16').click(function() { 
+	$('.js-overlay-campaign16').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign16');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign16').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//семнадцатое окно
+
+$('.js-button-campaign17').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign17').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign17').click(function() { 
+	$('.js-overlay-campaign17').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign17');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign17').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//восемнадцатое окно
+
+$('.js-button-campaign18').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign18').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign18').click(function() { 
+	$('.js-overlay-campaign18').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign18');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign18').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//девятнадцатое окно
+
+$('.js-button-campaign19').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign19').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign19').click(function() { 
+	$('.js-overlay-campaign19').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign19');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign19').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//двадцатое окно
+
+$('.js-button-campaign20').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign20').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign20').click(function() { 
+	$('.js-overlay-campaign20').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign20');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign20').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//двадцать первое окно
+
+$('.js-button-campaign21').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign21').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign21').click(function() { 
+	$('.js-overlay-campaign21').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign21');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign13').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//двадцать второе окно
+
+$('.js-button-campaign22').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign22').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign22').click(function() { 
+	$('.js-overlay-campaign22').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign22');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign13').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//двадцать третье окно
+
+$('.js-button-campaign23').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign23').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign23').click(function() { 
+	$('.js-overlay-campaign23').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign23');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign23').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//двадцать четвертое окно
+
+$('.js-button-campaign24').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign24').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign24').click(function() { 
+	$('.js-overlay-campaign24').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign24');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign24').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//двадцать пятое окно
+
+$('.js-button-campaign25').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign25').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign25').click(function() { 
+	$('.js-overlay-campaign25').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign25');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign25').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//двадцать шестое окно
+
+$('.js-button-campaign26').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign26').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign26').click(function() { 
+	$('.js-overlay-campaign26').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign26');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign26').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//двадцать седьмое окно
+
+$('.js-button-campaign27').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign27').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign27').click(function() { 
+	$('.js-overlay-campaign27').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign27');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign27').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//двадцать восьмое окно
+
+$('.js-button-campaign28').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign28').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign28').click(function() { 
+	$('.js-overlay-campaign28').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign28');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign28').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+
+//двадцать девятое окно
+
+$('.js-button-campaign29').click(function() { 
+	$('back').css('filter','blur(5px)');
+	$('.js-overlay-campaign29').fadeIn();
+	   /*$('.js-overlay-campaign').addClass('disabled');*/
+});
+
+// закрыть на крестик
+$('.js-close-campaign29').click(function() { 
+	$('.js-overlay-campaign29').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign29');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign29').fadeOut();
+        $('back').css('filter','none');
+		
+	}
+});
+/*// открыть по таймеру 
+$(window).on('load', function () { 
+	setTimeout(function(){
+		if($('.js-overlay-campaign').hasClass('disabled')) {
+			return false;
+		} else {
+			
+			$(".js-overlay-campaign").fadeIn();
+		}
+	}, 5000);
+});*/
+
